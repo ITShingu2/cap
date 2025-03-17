82d5e64b0e7df2d55ec62253bc6ad197478ba55f
Jin <tchilly0909@gmail.com>
2025-03-17 16:47:18 +0900

    25_03_17_1640i

--- begin/src/App.js
+++ begin/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from "./Header";
 function App() {
   return (
     <div>
-      <Header />
+      
       <main>
+        <h1>hi</h1>
       </main>
     </div>
   );
